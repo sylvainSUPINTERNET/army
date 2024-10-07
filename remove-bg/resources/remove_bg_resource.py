f478782c226da825fa4b8a93fd04e5e2fa40acc9
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-10-07 23:28:00 +0200

    wip

--- remove-bg/resources/remove_bg_resource.py
+++ remove-bg/resources/remove_bg_resource.py
@@ -22,14 +22,14 @@ async def whisper_transcribe(
         output_image, content_type = remove_bg_service.remove_bg(file, media_url, allowed_types)
         img_io = io.BytesIO()
 
-        if "png" in content_type or "PNG" in content_type:
+        if "png" in content_type or "PNG" in content_type or "jpg" in content_type or "JPG" in content_type or "jpeg" in content_type or "JPEG" in content_type:
             output_image.save(img_io, format='PNG')
             img_io.seek(0)
-        elif "jpeg" in content_type or "JPEG" in content_type:
-            if output_image.mode == "RGBA":
-                output_image = output_image.convert("RGB")  # Convertir en RGB
-            output_image.save(img_io, format='JPEG')
-            img_io.seek(0)
+        # elif "jpeg" in content_type or "JPEG" in content_type:
+        #     if output_image.mode == "RGBA":
+        #         output_image = output_image.convert("RGB")  # Convertir en RGB
+        #     output_image.save(img_io, format='JPEG')
+        #     img_io.seek(0)
         else:
             img_io.seek(0)
             raise Exception(f"Invalid content type: {content_type}")
