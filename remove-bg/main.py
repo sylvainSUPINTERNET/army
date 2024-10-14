1cf71a9e48528cdd7076139ca164ba4035b56bf3
sylvain <sylvain@macbook-pro-de-sylvain.home>
2024-10-15 00:14:13 +0200

    fk

--- remove-bg/main.py
+++ remove-bg/main.py
@@ -10,16 +10,10 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(threadName)s - %(levelname)s - %(message)s')
 
 app = FastAPI(title="remove-bg-api", version="0.1.0")
-allowedUiOrigin:str = os.getenv("CORS_UI_ORIGIN") != None and os.getenv("CORS_UI_ORIGIN") or "*"
-
-origins = [
-   # "*",  # Allows requests from any origin (use with caution in production)
-   allowedUiOrigin
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[os.getenv("CORS_UI_ORIGIN", "*") ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
